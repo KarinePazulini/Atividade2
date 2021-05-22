8e121966d1d0b7a93e1429667a7bfcc3e5474d8e
karinePazu <>
2021-05-22 01:44:13 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void func(int *ponteiro, int n, int &max, int &min){
+  for(int k=0; k<n; k++){
+    cout << *ponteiro << " ";
+    if(min > *ponteiro){
+      min = *ponteiro;
+    }
+    if(max < *ponteiro){
+      max = *ponteiro;
+    }
+    ponteiro++;
+  }
+  cout << endl;
+  cout << max << endl;
+  cout << min << endl;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int vetor[n-1];
+  int min;
+  int max;
+
+  for(int k=0; k<n; k++){
+    cin >> vetor[k];
+  }
+
+  max = vetor[0];
+  min = vetor[0];
+
+  cout << endl;
+
+  func(vetor, n, max, min);
+  
+}
